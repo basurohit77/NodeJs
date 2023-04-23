74c29078d4958a10e047bb1fd3d7358e390fe445
Rohit Basu <basurohit77@gmail.com>
2023-04-23 14:10:45 +0530

    NodeJs code

--- Day1/jsapp/922asyncawaitadd.js
+++ Day1/jsapp/922asyncawaitadd.js
@@ -6,6 +6,17 @@ async function test() {
     let res = await add(10, 20);
     console.log(res);
 }
+// With Promise
+// async function test() {
+//     let res =   add(10, 20).then((res) => {
+//         console.log(res);
+//     }, (err) => {
+//         console.log(err);
+//     });
+//     console.log("---test---");
+//     console.log(res);
+// }
+
 test();
 console.log('___________');
 console.log('___________');
